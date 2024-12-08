18759e7316f45f0ba43f5d3b02e00e688c523bad
mahfujarr <mahfujarrahmanjoy@gmail.com>
2024-12-08 12:18:26 +0600

    update

--- LMS.cpp
+++ LMS.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 void welcome();
+void studentLogin();
+void adminLogin();
+void createAccount();
+void listBooks();
 
 int main()
 {
@@ -13,4 +17,50 @@ void welcome()
     cout << "Welcome to Library Management System." << endl;
     cout << "Press any option below to begin: " << endl;
     cout << "1. Login as student\n2. Login as admin\n3. Create student account\n4. List all books" << endl;
+    cout << "---> ";
+    int x;
+    bool validInput = false;
+
+    while (!validInput)
+    {
+        cin >> x;
+        switch (x)
+        {
+        case 1:
+            studentLogin();
+            validInput = true;
+            break;
+        case 2:
+            adminLogin();
+            validInput = true;
+            break;
+        case 3:
+            createAccount();
+            validInput = true;
+            break;
+        case 4:
+            listBooks();
+            validInput = true;
+            break;
+        default:
+            cout << "Invalid input." << endl;
+            cout << "Please input from 1 to 4." << endl;
+            cout << "---> ";
+            break;
+        }
+    }
 }
+void studentLogin()
+{
+    cout << "Please enter your username: " << endl;
+    cout << "Please enter your password: " << endl;
+}
+void adminLogin() {
+
+};
+void createAccount() {
+
+};
+void listBooks() {
+
+};
